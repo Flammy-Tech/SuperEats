098211bc30da648ce1cc680d8931103ebbbbd102
Lewis Ng'ang'a <59686000+lewis-2000@users.noreply.github.com>
2023-12-03 18:38:02 +0300

    Added News Page
    Added news page with styling and all the assets

---
+++ public/scripts/tabs.js
@@ -0,0 +1,19 @@
+function openTab(tabName) {
+    // Hide all tabs
+    const tabs = document.querySelectorAll('.tab-content');
+    const categoriesLinks = document.querySelectorAll('.categories ul li');
+
+    tabs.forEach(tab => tab.style.display = 'none');
+
+    // Show the selected tab
+    document.getElementById(tabName).style.display = 'block';
+
+    // Add 'active' class to the corresponding tab link and remove from others
+    categoriesLinks.forEach(link => {
+        if (link.dataset.tab === tabName) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+}
