499ae3cf79de2c863df79bb8c1f206caae963981
Lewis Ng'ang'a <59686000+lewis-2000@users.noreply.github.com>
2023-12-06 18:25:58 +0300

    Cart Functionality
    Added Cart functionality

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,39 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const cardContainers = document.querySelectorAll('.food-card-container');
+    const cardContainers = document.querySelectorAll('.meal-card');
     const cartItems = [];
     var totalPrice = 0;
     localStorage.setItem('TotalPrice', totalPrice);
 
     cardContainers.forEach(cardContainer => {
-        const getFoodButtons = cardContainer.querySelectorAll('#get-food');
+        const getFoodButtons = cardContainer.querySelectorAll('.get-btn');
 
         getFoodButtons.forEach(getFoodButton => {
             getFoodButton.addEventListener('click', () => {
-                const foodId = getFoodButton.dataset.foodId;
-                const cartItemsContainer = document.getElementById('cart-items-container');
-                const cartCount = document.querySelector('#cart-count');
-                const foodName = cardContainer.querySelector('.food-title h2').textContent;
-                const foodPrice = cardContainer.querySelector('#food-price').textContent;
-                const foodDescription = cardContainer.querySelector('.food-description p').textContent;
-                const foodImage = cardContainer.querySelector('#food-image').src;
-                const foodVideo = cardContainer.querySelector('#food-video').src;
+                const mealId = parseInt(getFoodButton.dataset.mealId);
+                const foodName = cardContainer.querySelector('.meal-card-title h2').textContent;
+                const foodPrice = "YourPriceCalculationLogicHere"; // Modify this based on how you calculate the price
+                const foodDescription = cardContainer.querySelector('.meal-card-description p').textContent;
+                const foodImage = cardContainer.querySelector('.meal-card-image').src;
+                const foodVideo = cardContainer.querySelector('.meal-card-video').src;
 
-                const foodCard = {
-                    id: foodId,
-                    name: foodName,
-                    price: foodPrice,
-                    foodDescription: foodDescription,
-                    foodImage: foodImage,
-                    foodVideo: foodVideo,
-                    quantity: 1
-                };
+                const existingItemIndex = cartItems.findIndex(item => item.mealId === mealId);
 
-                addToCart(foodCard);
-                cartCount.innerHTML = ` ` + cartItemsContainer.childElementCount;
+                if (existingItemIndex !== -1) {
+                    // If item exists, update quantity
+                    console.log('Existing');
+                    cartItems[existingItemIndex].quantity += 1;
+                } else {
+                    // If item doesn't exist, add a new one
+                    console.log('Not Existing');
+                    const foodCard = {
+                        mealId: mealId,
+                        name: foodName,
+                        price: foodPrice,
+                        foodDescription: foodDescription,
+                        foodImage: foodImage,
+                        foodVideo: foodVideo,
+                        quantity: 1
+                    };
+                    cartItems.push(foodCard);
+                }
 
-                var singleFoodPrice = parseInt(foodPrice) * parseInt(document.querySelector('.cart-item-quantity').textContent);
-                totalPrice += singleFoodPrice;
-                localStorage.setItem('TotalPrice', totalPrice);
+                updateCartDisplay();
             });
         });
     });
@@ -53,24 +57,12 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    function addToCart(foodCard) {
-        const existingItem = cartItems.find(item => item.id === foodCard.id);
-
-        if (existingItem) {
-            existingItem.quantity += 1;
-        } else {
-            cartItems.push(foodCard);
-        }
-
-        updateCartDisplay();
-    }
-
     function updateCartDisplay() {
         const cartItemsContainer = document.getElementById('cart-items-container');
 
-        cartItemsContainer.innerHTML = '';
-
         if (cartItems.length > 0) {
+            cartItemsContainer.innerHTML = '';
+
             cartItems.forEach(item => {
                 const cartItemElement = document.createElement('div');
                 cartItemElement.classList.add('cart-item-card');
@@ -78,9 +70,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 cartItemElement.innerHTML = `
                     <div class="cart-item-image">
                         <img src="${item.foodImage}" alt="Image of the food in the cart">
-                    </div>
-                    <div class="cart-item-video">
-                        <video src="${item.foodVideo}" autoplay loop muted></video>
                     </div>
                     <div class="cart-item-name">${item.name}</div>
                     <div class="cart-item-description">${item.foodDescription}</div>
